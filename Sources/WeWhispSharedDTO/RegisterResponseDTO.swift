26ff4d4595740446a02109764ed3c21bc3c26afd
Carlo Contardi <c.contardi@icloud.com>
2025-12-12 15:54:03 +0100

    feat: add register and response

---
+++ Sources/WeWhispSharedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterResponseDTO.swift
+//  WeWhispSharedDTO
+//
+//  Created by Carlo Contardi on 12/12/25.
+//
+
+import Foundation
+public struct RegisterResponseDTO: Codable{
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+}
