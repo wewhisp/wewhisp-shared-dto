cb1844889714817dfa5a9c4b88cfe65ad663d708
Carlo Contardi <c.contardi@icloud.com>
2025-12-20 09:20:22 +0100

    feat(shared-fto): add LoginRequestDTO

---
+++ Sources/WeWhispSharedDTO/LoginRequestDTO.swift
@@ -0,0 +1,11 @@
+//
+//  LoginRequestDTO.swift
+//  WeWhispSharedDTO
+//
+//  Created by Carlo Contardi on 20/12/25.
+//
+
+struct LoginRequestDTO: Codable {
+    let email: String
+    let password: String
+}
