abec953b49b773b2369778876ab057577c5fe80a
Carlo Contardi <c.contardi@icloud.com>
2025-12-21 16:33:49 +0100

    feat(shared-dto): add ConnectionResponseAction

---
+++ Sources/WeWhispSharedDTO/ConnectionResponseAction.swift
@@ -0,0 +1,11 @@
+//
+//  ConnectionResponseAction.swift
+//  WeWhispSharedDTO
+//
+//  Created by Carlo Contardi on 21/12/25.
+//
+
+public enum ConnectionResponseAction: String, Codable {
+    case accept
+    case reject
+}
--- Sources/WeWhispSharedDTO/ConnectionResponseDTO.swift
+++ Sources/WeWhispSharedDTO/ConnectionResponseDTO.swift
@@ -5,5 +5,5 @@
 //  Created by Carlo Contardi on 20/12/25.
 //
 public struct ConnectionResponseDTO: Codable{
-    public let action: String // "accept" | "reject"
+    public let from: ConnectionResponseAction
 }
