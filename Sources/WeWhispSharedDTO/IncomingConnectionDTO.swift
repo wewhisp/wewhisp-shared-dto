b91435d2b2a7f74e7e625e8673233b1eb5c96c3a
Carlo Contardi <c.contardi@icloud.com>
2025-12-21 10:38:51 +0100

    IncomingConnectionDTO

--- Sources/WeWhispSharedDTO/IncomingConnectionDTO.swift
+++ Sources/WeWhispSharedDTO/IncomingConnectionDTO.swift
@@ -5,10 +5,10 @@
 //  Created by Carlo Contardi on 21/12/25.
 //
 import Foundation
-struct IncomingConnectionDTO: Identifiable, Codable, Sendable {
-    let id: UUID
-    let requesterId: UUID
-    let displayName: String
-    let avatarSeed: String
-    let requestedAt: Date
+public struct IncomingConnectionDTO: Identifiable, Codable, Sendable {
+    public let id: UUID
+    public let requesterId: UUID
+    public let displayName: String
+    public let avatarSeed: String
+    public let requestedAt: Date
 }
