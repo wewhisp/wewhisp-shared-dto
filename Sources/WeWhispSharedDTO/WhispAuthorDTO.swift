534d3c0e875c1107a0adbef6334e3078d8bde28f
Carlo Contardi <c.contardi@icloud.com>
2025-12-22 17:46:21 +0100

    refactor(dto): WhispAuthorDTO

--- Sources/WeWhispSharedDTO/WhispAuthorDTO.swift
+++ Sources/WeWhispSharedDTO/WhispAuthorDTO.swift
@@ -8,10 +8,12 @@
 import Foundation
 
 public struct WhispAuthorDTO: Codable, Sendable {
+    public let id: UUID
     public let displayName: String
     public let avatarSeed: String
     
-    public init(displayName: String, avatarSeed: String) {
+    public init(id: UUID, displayName: String, avatarSeed: String) {
+        self.id = id
         self.displayName = displayName
         self.avatarSeed = avatarSeed
     }
