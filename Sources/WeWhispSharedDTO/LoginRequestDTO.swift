4240b2cc3b9fba6308c205e174d0b2ab9f586971
Carlo Contardi <c.contardi@icloud.com>
2025-12-27 09:16:45 +0100

    refactor(shared-dto): update LoginRequestDTO and RegisterRequestDTO

--- Sources/WeWhispSharedDTO/LoginRequestDTO.swift
+++ Sources/WeWhispSharedDTO/LoginRequestDTO.swift
@@ -5,9 +5,10 @@
 //  Created by Carlo Contardi on 20/12/25.
 //
 
-public struct LoginRequestDTO: Codable {
+public struct LoginRequestDTO: Codable, Sendable {
     public let email: String
     public let password: String
+    
     public init(email: String, password: String) {
         self.email = email
         self.password = password
--- Sources/WeWhispSharedDTO/RegisterRequestDTO.swift
+++ Sources/WeWhispSharedDTO/RegisterRequestDTO.swift
@@ -5,8 +5,14 @@
 //  Created by Carlo Contardi on 20/12/25.
 //
 
+import Foundation
+
 public struct RegisterRequestDTO: Codable, Sendable {
     public let email: String
     public let password: String
     
+    public init(email: String, password: String) {
+        self.email = email
+        self.password = password
+    }
 }
